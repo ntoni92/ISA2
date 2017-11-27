df3350f80bd50cb31423f2df9d7f0d298ba5768c
GuidoBaccelli <32876385+GuidoBaccelli@users.noreply.github.com>
2017-11-27 11:11:23 +0100

    C++ file for generating Brent_Kung Adder

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main()
 {
     FILE* fp;
-    int i,N, N_BK;
+    int i,j,N, N_BK;
     char nome_file[20];
 
 	/*
@@ -18,7 +18,7 @@ int main()
     cout << endl;
 	*/
 
-    N = 12;
+    N = 8;
 
     for(i = 0; i < int(log2(N)); i++)
     {
@@ -34,17 +34,18 @@ int main()
 
         fp = fopen(nome_file, "w");
         fprintf(fp,"ENTITY BK_%dbit IS\n", N_BK);
-        fprintf(fp,"\tGENERIC(N: INTEGER := %d)\n", N_BK);
+        fprintf(fp,"\tGENERIC(N: INTEGER := %d);\n", N_BK);
         fprintf(fp,"\tPORT(\n");
-        fprintf(fp,"\t\tA: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
-        fprintf(fp,"\t\tB: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
-        fprintf(fp,"\t\tCIN : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tDOUT : OUT STD_LOGIC_VECTOR(N DOWNTO 0)\n\t);\n");
+        fprintf(fp,"\t\tG_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+        fprintf(fp,"\t\tP_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+        fprintf(fp,"\t\tG_out : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+        fprintf(fp,"\t\tP_out : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0)\n");
+        fprintf(fp,"\t);\n");
         fprintf(fp,"END ENTITY;\n\n");
 
         fprintf(fp,"ARCHITECTURE struct OF BK_%dbit IS\n\n", N_BK);
 
-        fprintf(fp,"COMPONENT AMPERSAND IS\n", N_BK);
+        fprintf(fp,"COMPONENT AMPERSAND IS\n");
         fprintf(fp,"\tPORT(\n");
         fprintf(fp,"\t\tGin0 : IN STD_LOGIC;\n");
         fprintf(fp,"\t\tPin0 : IN STD_LOGIC;\n");
@@ -55,38 +56,43 @@ int main()
         fprintf(fp,"\n\t);\n");
         fprintf(fp,"END COMPONENT;\n\n");
 
-        if ( i < int(log2(N)-1))
+        if ( i < int(log2(N)-1) )
         {
             fprintf(fp,"COMPONENT BK_%dbit IS\n", N_BK/2);
-            fprintf(fp,"\tGENERIC(N: INTEGER := %d)\n", N_BK/2);
+            fprintf(fp,"\tGENERIC(N: INTEGER := %d);\n", N_BK/2);
             fprintf(fp,"\tPORT(\n");
-            fprintf(fp,"\t\tA: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
-            fprintf(fp,"\t\tB: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
-            fprintf(fp,"\t\tCIN : IN STD_LOGIC;\n");
-            fprintf(fp,"\t\tDOUT : OUT STD_LOGIC_VECTOR(N DOWNTO 0)\n\t);\n");
+            fprintf(fp,"\t\tG_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+            fprintf(fp,"\t\tP_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+            fprintf(fp,"\t\tG_out : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+            fprintf(fp,"\t\tP_out : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0)\n");
+            fprintf(fp,"\t);\n");
             fprintf(fp,"END COMPONENT;\n\n");
         }
+        /*
+        if( i == 0 )
+        {
+            fprintf(fp,"COMPONENT GP_Gen IS\n");
+            fprintf(fp,"\tPORT(\n");
+            fprintf(fp,"\t\ta : IN STD_LOGIC;\n");
+            fprintf(fp,"\t\tb : IN STD_LOGIC;\n");
+            fprintf(fp,"\t\tGout : OUT STD_LOGIC;\n");
+            fprintf(fp,"\t\tPout : OUT STD_LOGIC\n");
+            fprintf(fp,"\t);\n");
+            fprintf(fp,"END COMPONENT;\n\n");
 
-        fprintf(fp,"COMPONENT GP_Gen IS\n", N_BK);
-        fprintf(fp,"\tPORT(\n");
-        fprintf(fp,"\t\ta : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tb : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tGout : OUT STD_LOGIC;\n");
-        fprintf(fp,"\t\tPout : OUT STD_LOGIC");
-        fprintf(fp,"\n\t);\n");
-        fprintf(fp,"END COMPONENT;\n\n");
-
-		fprintf(fp,"COMPONENT FA IS\n");
-        fprintf(fp,"\tPORT(\n");
-        fprintf(fp,"\t\ta : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tb : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tc_in : IN STD_LOGIC;\n");
-        fprintf(fp,"\t\tsum : OUT STD_LOGIC;\n");
-        fprintf(fp,"\t\tc_out : OUT STD_LOGIC");
-        fprintf(fp,"\n\t);\n");
-        fprintf(fp,"END COMPONENT;\n\n");
-
+            fprintf(fp,"COMPONENT FA IS\n");
+            fprintf(fp,"\tPORT(\n");
+            fprintf(fp,"\t\ta : IN STD_LOGIC;\n");
+            fprintf(fp,"\t\tb : IN STD_LOGIC;\n");
+            fprintf(fp,"\t\tc_in : IN STD_LOGIC;\n");
+            fprintf(fp,"\t\tsum : OUT STD_LOGIC;\n");
+            fprintf(fp,"\t\tc_out : OUT STD_LOGIC\n");
+            fprintf(fp,"\t);\n");
+            fprintf(fp,"END COMPONENT;\n\n");
+        }
+        */
         fprintf(fp,"BEGIN\n\n");
+        /*
         fprintf(fp,"\tSIGNAL G_array : STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
         fprintf(fp,"\tSIGNAL P_array : STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
         fprintf(fp,"\n");
@@ -98,13 +104,41 @@ int main()
         fprintf(fp,"\t\t\tPout => P_array(i)\n");
         fprintf(fp,"\t\t);\n");
         fprintf(fp,"\tEND GENERATE;\n\n");
+        */
 
-		
-	if (N == N_BK)
-	{
-		// GENERATE FULL ADDERS
-	}
-	 
+        fprintf(fp,"\tSIGNAL Gin_lv2 : STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+        fprintf(fp,"\tSIGNAL Pin_lv2 : STD_LOGIC_VECTOR(N-1 DOWNTO 0);\n");
+
+        //fprintf(fp,"\tFirst_Row_AMP_OP:FOR i IN 0 TO N-1 GENERATE\n");
+        //fprintf(fp,"\t\t AMP_OP_Gen: IF ((2*i)+1)<N GENERATE\n");
+        j = 0;
+        while(2*j+1 < N_BK)
+        {
+            fprintf(fp,"\n");
+            fprintf(fp,"\tAMP_OP_%d: AMPERSAND PORT MAP(\n",2*j+1);
+            fprintf(fp,"\t\tGin0 => G_in(%d),\n", 2*j);
+            fprintf(fp,"\t\tPin0 => P_in(%d),\n", 2*j);
+            fprintf(fp,"\t\tGin1 => G_in(%d),\n", 2*j+1);
+            fprintf(fp,"\t\tPin1 => P_in(%d)\n", 2*j+1);
+            fprintf(fp,"\t\tGout => Gin_lv2(%d),\n", 2*j+1);
+            fprintf(fp,"\t\tPout => Pin_lv2((%d),\n", 2*j+1);
+            fprintf(fp,"\t);\n");
+
+            j++;
+        }
+
+        fprintf(fp,"\n");
+
+        j = 0;
+        while(2*j < N_BK)
+        {
+            fprintf(fp,"\tGin_lv2(%d) <= G_in(%d);\n", 2*j,2*j);
+            fprintf(fp,"\tPin_lv2(%d) <= P_in(%d);\n",2*j,2*j);
+
+            j++;
+        }
+
+        fprintf(fp,"\n");
         fprintf(fp,"END struct;\n");
 
         fclose(fp);
