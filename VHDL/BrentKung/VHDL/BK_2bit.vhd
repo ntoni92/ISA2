7eda02e73a1ad90febeaa96a8fda43a513c2a85a
ntoni92 <antonio.orlandoo@hotmail.it>
2017-11-28 11:12:56 +0100

    Brent-Kung adder

---
+++ VHDL/BrentKung/VHDL/BK_2bit.vhd
@@ -0,0 +1,46 @@
+ENTITY BK_2bit IS
+	GENERIC(N: INTEGER := 2);
+	PORT(
+		G_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		P_in: IN STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		G_out: OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+		P_out : OUT STD_LOGIC_VECTOR(N-1 DOWNTO 0)
+	);
+END ENTITY;
+
+ARCHITECTURE struct OF BK_2bit IS
+
+COMPONENT AMPERSAND IS
+	PORT(
+		Gin0 : IN STD_LOGIC;
+		Pin0 : IN STD_LOGIC;
+		Gin1 : IN STD_LOGIC;
+		Pin1 : IN STD_LOGIC;
+		Gout : OUT STD_LOGIC;
+		Pout : OUT STD_LOGIC
+	);
+END COMPONENT;
+
+BEGIN
+
+	SIGNAL Gin_lv2 : STD_LOGIC_VECTOR((N/2)-1 DOWNTO 0);
+	SIGNAL Pin_lv2 : STD_LOGIC_VECTOR((N/2)-1 DOWNTO 0);
+	SIGNAL GoutBK_lv2 : STD_LOGIC_VECTOR((N/2)-1 DOWNTO 0);
+	SIGNAL PoutBK_lv2 : STD_LOGIC_VECTOR((N/2)-1 DOWNTO 0);
+	SIGNAL Gout_lv2 : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+	SIGNAL Pout_lv2 : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+
+	AMP_OP_1: AMPERSAND PORT MAP(
+		Gin0 => G_in(0),
+		Pin0 => P_in(0),
+		Gin1 => G_in(1),
+		Pin1 => P_in(1)
+		Gout => Gin_lv2(0),
+		Pout => Pin_lv2(0),
+	);
+	G_out(0) <= G_in(0);
+	P_out(0) <= P_in(0);
+
+	Gout(1) <= Gin_lv2(0);
+	Pout(1) <= Pin_lv2(0);
+END struct;
