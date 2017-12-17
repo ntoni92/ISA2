d1eba9c0299367ee64ec8dd14a3bdfd42b887a9d
Giuseppe B <meaepeppe@gmail.com>
2017-12-17 18:24:22 +0100

    test super speed
    tt

--- VHDL/MBE_dadda_mult/tb/tb_dadda_mult_basic.vhd
+++ VHDL/MBE_dadda_mult/tb/tb_dadda_mult_basic.vhd
@@ -20,7 +20,7 @@ END component;
 
 BEGIN
 	
-DUT: 	ENTITY work.MBE_dadda_mult_9x9(almost_super_speed)
+DUT: 	ENTITY work.MBE_dadda_mult_9x9(super_speed)
 	PORT MAP(	A => A, 
 			B => B,
 			m_out => m_out);
